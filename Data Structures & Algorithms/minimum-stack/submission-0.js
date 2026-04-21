d218fddac847a27c7be52a456356992436dc929a
Hiago Bernardi <57834053+HiagoBernardi1992@users.noreply.github.com>
2026-04-21 02:17:24 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,38 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        let min = Infinity;
+        for (let num of this.stack) {
+            min = Math.min(min, num);
+        }
+        return min;
+    }
+}
