43cf32c5eca60732d399cd6d33738ef469f0ea40
Hiago Bernardi <57834053+HiagoBernardi1992@users.noreply.github.com>
2026-06-08 07:54:07 -0700

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        q = deque()
+
+        # 1. multi-source: adiciona todos os tesouros (0)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    q.append((r, c))
+
+        # 2. BFS
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while q:
+            r, c = q.popleft()
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if (
+                    0 <= nr < ROWS and
+                    0 <= nc < COLS and
+                    grid[nr][nc] == 2147483647
+                ):
+                    grid[nr][nc] = grid[r][c] + 1
+                    q.append((nr, nc))
+
+        
