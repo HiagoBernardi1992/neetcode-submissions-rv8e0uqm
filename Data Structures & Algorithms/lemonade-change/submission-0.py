1a32ba63eb9dd1ccced60e2eb886394c68b55774
Hiago Bernardi <57834053+HiagoBernardi1992@users.noreply.github.com>
2026-07-18 18:11:04 +0100

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        mine = 0
+        for val in bills:
+            if val > 5:
+                give_back = val - 5
+                mine -= give_back
+                if mine < 0:
+                    return False
+                
+            mine += 5
+
+        return True
