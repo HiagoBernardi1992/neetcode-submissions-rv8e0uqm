5bbd0bb96e20c80be2542459a1a70a861c46f15f
Hiago Bernardi <57834053+HiagoBernardi1992@users.noreply.github.com>
2026-07-21 16:27:14 +0100

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # self.cheapest = float("inf")
+
+        # adj = {i: [] for i in range(n)}
+        # for f, t, price in flights:
+        #     adj[f].append((t, price))
+
+        # visited = set()
+
+        # def dfs(node, cost, flights_used):
+        #     if cost >= self.cheapest:
+        #         return
+
+        #     if flights_used > k + 1:
+        #         return
+
+        #     if node == dst:
+        #         self.cheapest = min(self.cheapest, cost)
+        #         return
+
+        #     visited.add(node)
+
+        #     for nei, price in adj[node]:
+        #         if nei not in visited:
+        #             dfs(nei, cost + price, flights_used + 1)
+
+        #     visited.remove(node)   # backtracking
+
+        # dfs(src, 0, 0)
+
+        # return self.cheapest if self.cheapest != float("inf") else -1
+        prices = [float('inf')] * n
+        prices[src] = 0
+        for i in range(k + 1):
+            temp = prices.copy()
+            for f, t, p in flights:
+                if prices[f] == float("inf"):
+                    continue
+                if prices[f] + p < temp[t]:
+                    temp[t] = prices[f] + p
+            prices = temp
+
+        return prices[dst] if prices[dst] != float('inf') else -1
